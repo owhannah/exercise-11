1fc1a3fefea31b6cf1a0f7318011ad432a0dc2ff
Your Name <you@example.com>
2024-01-24 20:53:38 +0800

    write_a_program_to_extract_each_digit_from_an_integer_in_the_reverse_order

--- write_a_program_to_extract_each_digit_from_an_integer_in_the_reverse_order.py
+++ write_a_program_to_extract_each_digit_from_an_integer_in_the_reverse_order.py
@@ -1,1 +1,15 @@
 # Write a Program to extract each digit from an integer in the reverse order
+
+number = 7536
+print("Given number:", number)
+
+# Iterate through each digit in reverse order
+while number > 0:
+    # Get the last digit
+    last_digit = number % 10
+    
+    # Remove the last digit and repeat the loop
+    number //= 10
+    
+    # Print the current digit
+    print(last_digit, end=" ")
